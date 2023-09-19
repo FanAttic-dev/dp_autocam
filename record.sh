82e3891fc9c5aa6fa4d8c75cd99e26b4e578f716
Attila Zsiros <attila.zsiros@mail.muni.cz>
2023-09-19 18:49:55 +0200

    update record script

--- record.sh
+++ record.sh
@@ -1,6 +1,6 @@
-VIDEOS_PATH='../../datasets/TrnavaZilina/videos/clips'
+VIDEOS_PATH='../../datasets/TrnavaZilina/videos/first_half_clips'
 CONFIG_PATH=$VIDEOS_PATH/../../config.json
 for clip in $VIDEOS_PATH/*.mp4 
 do
-    nice -n 20 python index.py --video-path $clip --config-path $CONFIG_PATH --record --hide-windows
+    python index.py --video-path $clip --config-path $CONFIG_PATH --record --hide-windows
 done
