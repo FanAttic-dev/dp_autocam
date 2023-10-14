correct rotation

src/camera/projective_camera.py
@@ -7,7 +7,7 @@ from utils.config import Config
 from utils.constants import Color
 from filters.kalman_filter import KalmanFilterVel
 from filters.particle_filter import ParticleFilter
-from utils.helpers import apply_homography, discard_extreme_boxes_, filter_bbs_ball, get_bounding_box, get_pitch_rotation_rad, points_average, discard_extreme_points_, get_bb_center, lies_in_rectangle, points_variance, rotate_pts
+from utils.helpers import apply_homography, discard_extreme_boxes_, filter_bbs_ball, get_bounding_box, get_pitch_rotation_rad, points_average, discard_extreme_points_, get_bb_center, lies_in_rectangle, points_variance
 
 
 class ProjectiveCamera(Camera):
@@ -222,23 +222,9 @@ class ProjectiveCamera(Camera):
         ...
 
     @property
+    @abstractmethod
     def H(self):
-        src = self.get_corner_pts()
-        dst = Camera.FRAME_CORNERS
-
-        H, _ = cv2.findHomography(src, dst)
-
-        if Config.autocam["correct_rotation"]:
-            # TODO: use lookup table
-            pitch_coords_orig = self.config.pitch_coords_pts
-            pitch_coords_frame = cv2.perspectiveTransform(
-                pitch_coords_orig.astype(np.float64), H)
-            roll_rad = get_pitch_rotation_rad(pitch_coords_frame)
-
-            src = np.array(rotate_pts(src, roll_rad), dtype=np.int32)
-            H, _ = cv2.findHomography(src, dst)
-
-        return H
+        ...
 
     @property
     def H_inv(self):

src/camera/spherical_camera.py
@@ -5,6 +5,7 @@ from camera.camera import Camera
 from camera.projective_camera import ProjectiveCamera
 from utils.config import Config
 from utils.constants import INTERPOLATION_TYPE, Color, DrawingMode
+from utils.helpers import get_pitch_rotation_rad, rotate_pts
 
 
 class SphericalCamera(ProjectiveCamera):
@@ -86,6 +87,14 @@ class SphericalCamera(ProjectiveCamera):
     def fov_vert_deg(self):
         return self.fov_horiz_deg / Camera.FRAME_ASPECT_RATIO
 
+    @property
+    def H(self):
+        src = self.get_corner_pts()
+        dst = Camera.FRAME_CORNERS
+
+        H, _ = cv2.findHomography(src, dst)
+        return H
+
     def get_corner_pts(self):
         coords = self.coords_screen_fov * self.frame_orig_size
         coords = np.reshape(coords, (Camera.FRAME_H, Camera.FRAME_W, 2))
@@ -147,7 +156,17 @@ class SphericalCamera(ProjectiveCamera):
         return cv2.remap(frame_orig, map_x, map_y, interpolation=INTERPOLATION_TYPE)
 
     def get_frame(self, frame_orig):
-        return self._remap(frame_orig, self.coords_screen_fov)
+        coords = self.coords_screen_fov
+
+        if Config.autocam["correct_rotation"]:
+            pitch_coords_orig = self.config.pitch_coords_pts.astype(np.float32)
+            pitch_coords_frame = cv2.perspectiveTransform(
+                pitch_coords_orig, self.H
+            )
+            roll_rad = get_pitch_rotation_rad(pitch_coords_frame)
+            coords = rotate_pts(coords, roll_rad, center=(0, 0))
+
+        return self._remap(frame_orig, coords)
 
     def get_roi_border_pts(self, skip=50):
         coords = self.coords_screen_fov

src/utils/helpers.py
@@ -129,18 +129,22 @@ def get_bb_center(bb):
     return x, y
 
 
-def rotate_pts(pts, angle_rad):
-    center_x, center_y = np.mean(pts, axis=0)
-    pts_rot = []
-    for x, y in pts:
-        qx = center_x + \
-            math.cos(angle_rad) * (x - center_x) - \
-            math.sin(angle_rad) * (y - center_y)
-        qy = center_y + \
-            math.sin(angle_rad) * (x - center_x) + \
-            math.cos(angle_rad) * (y - center_y)
-        pts_rot.append([qx, qy])
-    return pts_rot
+def rotate_pts(pts, angle_rad, center=None):
+    if center is None:
+        center = np.mean(pts, axis=0)
+
+    center_x, center_y = center
+    angle_cos = math.cos(angle_rad)
+    angle_sin = math.sin(angle_rad)
+
+    pts[:, 0] = center_x + \
+        angle_cos * (pts[:, 0] - center_x) - \
+        angle_sin * (pts[:, 1] - center_y)
+    pts[:, 1] = center_y + \
+        angle_sin * (pts[:, 0] - center_x) + \
+        angle_cos * (pts[:, 1] - center_y)
+
+    return pts
 
 
 def get_pitch_rotation_rad(pts):