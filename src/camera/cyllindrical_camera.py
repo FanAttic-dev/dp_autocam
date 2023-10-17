adjust frame splitter params, fix rotation correction

src/camera/cyllindrical_camera.py
@@ -8,9 +8,9 @@ import utils.utils as utils
 
 
 class CyllindricalCamera(ProjectiveCamera):
-    def __init__(self, frame_orig, config: Config):
-        self.cyllinder_radius = 1860
-        super().__init__(frame_orig, config)
+    def __init__(self, frame_orig, config: Config, ignore_bounds=Config.autocam["debug"]["ignore_bounds"]):
+        self.cyllinder_radius = config.dataset["camera_params"]["cyllinder_radius"]
+        super().__init__(frame_orig, config, ignore_bounds)
 
     def ptz2coords(self, pan_deg, tilt_deg, f=None):
         pan_rad = np.deg2rad(pan_deg)
@@ -38,38 +38,20 @@ class CyllindricalCamera(ProjectiveCamera):
             "right top": [self.fov_horiz_deg / 2, -self.fov_vert_deg / 2],
         }
 
-    def get_corner_pts(self):
+    def get_corner_pts(self, correct_rotation):
         pts = [
             self.ptz2coords(
                 self.pan_deg + pan_deg,
                 self.tilt_deg + tilt_deg)
             for pan_deg, tilt_deg in self.corners_ang.values()
         ]
-        return np.array(pts, dtype=np.int32)
+        pts = np.array(pts, dtype=np.int32)
 
-    @property
-    def H_inv(self):
-        return np.linalg.inv(self.H)
+        if not correct_rotation:
+            return pts
 
-    @property
-    def H(self):
-        src = self.get_corner_pts()
-        dst = Camera.FRAME_CORNERS
-
-        H, _ = cv2.findHomography(src, dst)
-        if Config.autocam["correct_rotation"]:
-            src = self.correct_rotation(src, H)
-            H, _ = cv2.findHomography(src, dst)
-
-        return H
-
-    def correct_rotation(self, pts, H):
-        pitch_coords_orig = self.config.pitch_coords_pts
-        pitch_coords_frame = cv2.perspectiveTransform(
-            pitch_coords_orig.astype(np.float64), H)
-        roll_rad = utils.get_pitch_rotation_rad(pitch_coords_frame)
-
-        return np.array(utils.rotate_pts(pts, roll_rad), dtype=np.int32)
+        _, pts = self.correct_rotation(pts)
+        return pts
 
     def get_frame(self, frame_orig):
         return cv2.warpPerspective(
@@ -90,12 +72,12 @@ class CyllindricalCamera(ProjectiveCamera):
         return pts.astype(np.int16)
 
     def draw_roi_(self, frame_orig, color=Color.VIOLET):
-        pts = self.get_corner_pts()
-        cv2.polylines(frame_orig, [pts], True, color, thickness=10)
+        pts = self.get_corner_pts(Config.autocam["correct_rotation"])
+        cv2.polylines(frame_orig, [pts], True, color, thickness=5)
 
     def draw_frame_mask(self, frame_orig):
         mask = np.zeros(frame_orig.shape[:2], dtype=np.uint8)
-        pts = self.get_corner_pts()
+        pts = self.get_corner_pts(Config.autocam["correct_rotation"])
         cv2.fillPoly(mask, [pts], 255)
         return cv2.bitwise_and(frame_orig, frame_orig, mask=mask)
 

src/camera/projective_camera.py
@@ -11,17 +11,18 @@ import utils.utils as utils
 
 
 class ProjectiveCamera(Camera):
-    PAN_DX = 1
-    TILT_DY = 1
-    ZOOM_DZ = 1
+    PAN_DX = .1
+    TILT_DY = .1
+    ZOOM_DZ = .5
 
-    def __init__(self, frame_orig, config: Config):
+    def __init__(self, frame_orig, config: Config, ignore_bounds=Config.autocam["debug"]["ignore_bounds"]):
         h, w, _ = frame_orig.shape
         self.frame_orig_size = np.array([w, h], dtype=np.uint16)
         self.frame_orig_center_x = w // 2
         self.frame_orig_center_y = h // 2
 
         self.config = config
+        self.ignore_bounds = ignore_bounds
         self.sensor_w = Camera.SENSOR_W
         self.lens_fov_horiz_deg = config.dataset["camera_params"]["lens_fov_horiz_deg"]
 
@@ -48,7 +49,7 @@ class ProjectiveCamera(Camera):
         self.pid_f.update(pid_f)
 
     def check_ptz(self, pan_deg, tilt_deg, zoom_f):
-        if Config.autocam["debug"]["ignore_bounds"]:
+        if self.ignore_bounds:
             return True
 
         is_valid = pan_deg >= self.pan_deg_min and pan_deg <= self.pan_deg_max and \
@@ -72,10 +73,22 @@ class ProjectiveCamera(Camera):
         return pan_deg, tilt_deg, zoom_f
 
     def check_corner_pts(self):
-        corner_pts = self.get_corner_pts()
+        corner_pts = self.get_corner_pts(Config.autocam["correct_rotation"])
         w, h = self.frame_orig_size
         frame_box = np.array([0, 0, w-1, h-1])
         return utils.polygon_lies_in_box(corner_pts, frame_box)
+
+    def correct_rotation(self, pts, center=None):
+        corner_pts = self.get_corner_pts(correct_rotation=False)
+        H, _ = cv2.findHomography(corner_pts, Camera.FRAME_CORNERS)
+
+        pitch_coords_orig = self.config.pitch_coords_pts.astype(np.float32)
+        pitch_coords_frame = cv2.perspectiveTransform(
+            pitch_coords_orig, H
+        )
+
+        roll_rad = utils.get_pitch_rotation_rad(pitch_coords_frame)
+        return H, utils.rotate_pts(pts, roll_rad, center)
 
     def set_ptz(self, pan_deg, tilt_deg, zoom_f):
         self.pan_deg, self.tilt_deg, self.zoom_f = pan_deg, tilt_deg, zoom_f
@@ -188,9 +201,10 @@ class ProjectiveCamera(Camera):
         return self.try_set_ptz(*ptz)
 
     @property
-    @abstractmethod
     def H(self):
-        ...
+        corner_pts = self.get_corner_pts(Config.autocam["correct_rotation"])
+        H, _ = cv2.findHomography(corner_pts, Camera.FRAME_CORNERS)
+        return H
 
     @property
     def H_inv(self):
@@ -230,7 +244,7 @@ class ProjectiveCamera(Camera):
     def draw_dead_zone_(self, frame):
         start, end = self.dead_zone
         cv2.rectangle(frame, start, end,
-                      color=Color.RED, thickness=1)
+                      color=Color.RED, thickness=5)
 
     @abstractmethod
     def draw_frame_mask(self, frame_orig):

src/camera/spherical_camera.py
@@ -9,12 +9,11 @@ import utils.utils as utils
 
 
 class SphericalCamera(ProjectiveCamera):
-    ZOOM_DZ = 5
     SENSOR_DX = 10
     FOV_DX = 5
 
-    def __init__(self, frame_orig, config: Config):
-        super().__init__(frame_orig, config)
+    def __init__(self, frame_orig, config: Config, ignore_bounds=Config.autocam["debug"]["ignore_bounds"]):
+        super().__init__(frame_orig, config, ignore_bounds)
 
     @property  # TODO: make cached
     def lens_fov_vert_deg(self):
@@ -74,15 +73,7 @@ class SphericalCamera(ProjectiveCamera):
         coords_screen = self._spherical2screen(coords_spherical)
         return (coords_screen * self.frame_orig_size).astype(np.int16)
 
-    @property
-    def H(self):
-        src = self.get_corner_pts()
-        dst = Camera.FRAME_CORNERS
-
-        H, _ = cv2.findHomography(src, dst)
-        return H
-
-    def get_corner_pts(self):
+    def get_corner_pts(self, correct_rotation):
         coords = self.coords_screen_fov * self.frame_orig_size
         coords = np.reshape(coords, (Camera.FRAME_H, Camera.FRAME_W, 2))
 
@@ -91,15 +82,21 @@ class SphericalCamera(ProjectiveCamera):
         rb = coords[-1, -1]
         rt = coords[0, -1]
 
-        return np.array([lt, lb, rb, rt], dtype=np.int32)
+        pts = np.array([lt, lb, rb, rt], dtype=np.int32)
+
+        if not correct_rotation:
+            return pts
+
+        _, pts = self.correct_rotation(pts)
+        return pts
 
     def roi2original(self, coords_screen_roi):
         frame_size = np.array([Camera.FRAME_W, Camera.FRAME_H], dtype=np.int16)
         coords_screen = coords_screen_roi / frame_size
 
         if Config.autocam["correct_rotation"]:
-            coords_screen = utils.rotate_pts(
-                coords_screen, self.roll_rad,
+            _, coords_screen = self.correct_rotation(
+                coords_screen,
                 center=np.array([0.5, 0.5], dtype=np.float32)
             )
 
@@ -112,19 +109,11 @@ class SphericalCamera(ProjectiveCamera):
         coords_screen = (coords_screen * self.frame_orig_size).astype(np.int16)
         return coords_screen
 
-    def correct_rotation(self, coords, center=None):
-        pitch_coords_orig = self.config.pitch_coords_pts.astype(np.float32)
-        pitch_coords_frame = cv2.perspectiveTransform(
-            pitch_coords_orig, self.H
-        )
-        self.roll_rad = utils.get_pitch_rotation_rad(pitch_coords_frame)
-        return utils.rotate_pts(coords, self.roll_rad, center)
-
     def get_frame(self, frame_orig):
         coords = self.coords_screen_fov
 
         if Config.autocam["correct_rotation"]:
-            coords = self.correct_rotation(coords)
+            _, coords = self.correct_rotation(coords)
 
         return self._remap(frame_orig, coords)
 
@@ -209,7 +198,7 @@ class SphericalCamera(ProjectiveCamera):
         if drawing_mode == DrawingMode.LINES:
             skip = 5
             coords = self.get_roi_border_pts(skip)
-            cv2.polylines(frame_orig, [coords], True, color, thickness=10)
+            cv2.polylines(frame_orig, [coords], True, color, thickness=5)
         elif drawing_mode == DrawingMode.CIRCLES:
             skip = 50
             coords = self.get_roi_border_pts(skip)
@@ -256,7 +245,7 @@ class SphericalCamera(ProjectiveCamera):
         y2 = self.frame_orig_center_y + target_h // 2
 
         cv2.rectangle(frame_orig, (x1, y1), (x2, y2),
-                      color=Color.ORANGE, thickness=10)
+                      color=Color.ORANGE, thickness=5)
 
     def process_input(self, key, mouseX, mouseY):
         is_alive = True

src/camera/top_down.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from camera.camera import Camera
 from detection.detector import YoloDetector
+from utils.config import Config
 from utils.constants import INTERPOLATION_TYPE, Color
 import utils.utils as utils
 
@@ -118,7 +119,7 @@ class TopDown:
         x_max, y_max = np.max(pitch_pts, axis=0)[0] + margin
 
         pts_warped = []
-        for x, y in self.camera.get_corner_pts():
+        for x, y in self.camera.get_corner_pts(Config.autocam["correct_rotation"]):
             x = np.clip(x, x_min, x_max)
             y = np.clip(y, y_min, y_max)
             x_, y_ = utils.apply_homography(self.H, x, y)