85b155f94244051cde391f4c89ff4e946b6eec9a
Attila Zsiros <attila.zsiros@mail.muni.cz>
2023-10-20 11:58:38 +0200

    record full match

--- scripts/record_full_match.sh
+++ scripts/record_full_match.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CONFIG_PATH='./configs/config_trnava_zilina.yaml'
-for clip in main_p1.mp4 # main_p1.mp4
+for clip in main_p0.mp4 main_p1.mp4
 do
     python src/main.py --video-name $clip --config-path $CONFIG_PATH --record --hide-windows --export-frames
 done
