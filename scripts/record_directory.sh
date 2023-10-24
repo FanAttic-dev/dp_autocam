48d986a65ba05324b2ade4878462084c00446a34
Attila Zsiros <attila.zsiros@mail.muni.cz>
2023-10-24 10:55:21 +0200

    scripts: add timers

--- scripts/record_directory.sh
+++ scripts/record_directory.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
 CONFIG_PATH='./configs/config_trnava_zilina.yaml'
 DIR_PATH='../../datasets/TrnavaZilina/main/clips'
-for clip_path in $DIR_PATH/*.mp4
-do
-    video_name=${clip_path##*/}
-    python src/main.py --video-name $video_name --config-path $CONFIG_PATH --record --hide-windows --export-frames
-done
+
+time {
+    for clip_path in $DIR_PATH/*.mp4
+    do
+        video_name=${clip_path##*/}
+        python src/main.py --video-name $video_name --config-path $CONFIG_PATH --record --hide-windows --export-frames
+    done
+}
--- scripts/record_full_match.sh
+++ scripts/record_full_match.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-CONFIG_PATH='./configs/config_trnava_zilina.yaml'
-for clip in main_p0.mp4 main_p1.mp4
-do
-    echo "Running $clip in parallel"
-    python src/main.py --video-name $clip --config-path $CONFIG_PATH --record --hide-windows --export-frames &
-done
 
-wait
-echo "All done"
+CONFIG_PATH='./configs/config_trnava_zilina.yaml'
+
+time {
+    for clip in main_p0.mp4 main_p1.mp4
+    do
+        echo "Running $clip in parallel"
+        python src/main.py --video-name $clip --config-path $CONFIG_PATH --record --hide-windows --export-frames &
+    done
+    wait
+}
