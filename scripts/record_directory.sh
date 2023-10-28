70a5136dfb2e84464dfbf745e0f7f9ff162992ce
Attila Zsiros <attila.zsiros@mail.muni.cz>
2023-10-28 11:50:20 +0200

    redirect error to log file

--- scripts/record_directory.sh
+++ scripts/record_directory.sh
@@ -7,6 +7,6 @@ time {
     for clip_path in $DIR_PATH/*.mp4
     do
         video_name=${clip_path##*/}
-        python src/main.py --video-name $video_name --config-path $CONFIG_PATH --record --hide-windows --export-frames
+        python src/main.py --video-name $video_name --config-path $CONFIG_PATH --record --hide-windows --export-frames 2> ./recordings/${video_name}_err_log.txt
     done
 }
--- scripts/record_full_match.sh
+++ scripts/record_full_match.sh
@@ -6,7 +6,7 @@ time {
     for clip in main_p0.mp4 main_p1.mp4
     do
         echo "Running $clip in parallel"
-        python src/main.py --video-name $clip --config-path $CONFIG_PATH --record --hide-windows --export-frames &
+        python src/main.py --video-name $clip --config-path $CONFIG_PATH --record --hide-windows --export-frames 2> ./recordings/${clip}_err_log.txt &
     done
     wait
 }
