6650d6f6e9721ef1700efe1863e3ca7ad7a243ba
Attila Zsiros <attila.zsiros@mail.muni.cz>
2023-10-29 21:18:52 +0100

    record in parallel

--- scripts/record_full_match.sh
+++ scripts/record_full_match.sh
@@ -4,7 +4,7 @@ time {
     for clip in main_p0.mp4 main_p1.mp4
     do
         echo "Running $clip in parallel"
-        python src/main.py --video-name $clip --record --output-sub-dir full --export-interval-sec 10 --hide-windows
+        python src/main.py --video-name $clip --record --output-sub-dir full --export-interval-sec 10 --hide-windows &
     done
     wait
 }
